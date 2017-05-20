890392cd8b1e4a84d0fd4928574cd148c809a165
Benjamin Peters <bpeters@coli.uni-saarland.de>
2017-05-20 10:56:24 +0200

    Changed misleading language selection names

--- tools/wiktionary.py
+++ tools/wiktionary.py
@@ -8,8 +8,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from os.path import join
 
-TRAINING_DATA_PATH = '/home/bpop/thesis/mg2p/data/deri-knight/pron_data/gold_data_train'
+WIKTIONARY_TRAINING_PATH = '/home/bpop/thesis/mg2p/data/deri-knight/pron_data/gold_data_train'
 TEST_DATA_PATH = '/home/bpop/thesis/mg2p/data/deri-knight/pron_data/gold_data_test'
+IPAHELP_PATH = '/home/bpop/thesis/mg2p/data/deri-knight/ipa_help/all.g-to-ipa.cleaned.table'
 
 HIGH_RESOURCE = ['ady', 'afr', 'ain', 'amh', 'ang', 'ara', 'arc', 'ast', 
                 'aze', 'bak', 'ben', 'bre', 'bul', 'cat', 'ces', 'cym', 
@@ -23,29 +24,37 @@ HIGH_RESOURCE = ['ady', 'afr', 'ain', 'amh', 'ang', 'ara', 'arc', 'ast',
                 'syc', 'tel', 'tgk', 'tgl', 'tha', 'tur', 'ukr', 'urd', 
                 'vie', 'vol', 'yid', 'yue', 'zho']
                 
-LOW_RESOURCE = ['aar', 'abk', 'abq', 'ace', 'ach', 'agr', 'aka', 'akl', 
-                'akz', 'ale', 'alt', 'ami', 'aqc', 'arg', 'arw', 'arz', 
-                'asm', 'ava', 'aym', 'bal', 'bam', 'bcl', 'bel', 'bis', 
-                'bod', 'bos', 'bua', 'bug', 'ceb', 'cha', 'che', 'chk', 
-                'chm', 'cho', 'chv', 'cic', 'cjs', 'cor', 'crh', 'dar', 
-                'est', 'ewe', 'fij', 'fil', 'frr', 'fry', 'fur', 'gaa', 
-                'gag', 'glg', 'grc', 'grn', 'gsw', 'guj', 'hak', 'hat', 
-                'hau', 'haw', 'hil', 'hit', 'hrv', 'iba', 'ilo', 'ind', 
-                'inh', 'jam', 'jav', 'kaa', 'kab', 'kal', 'kan', 'kaz', 
-                'kea', 'ket', 'khb', 'kin', 'kir', 'kjh', 'kom', 'kum', 
-                'lin', 'lld', 'lug', 'luo', 'lus', 'lzz', 'mah', 'mal', 
-                'mar', 'mlg', 'mnk', 'mns', 'moh', 'mon', 'mri', 'mus', 
-                'mww', 'myv', 'mzn', 'nah', 'nap', 'nau', 'nds', 'nep', 
-                'new', 'nia', 'niu', 'non', 'nor', 'nso', 'oss', 'osx', 
-                'pag', 'pam', 'pan', 'pau', 'pon', 'ppl', 'prs', 'que', 
-                'roh', 'rom', 'rtm', 'ryu', 'sac', 'sah', 'sat', 'sei', 
-                'sme', 'sna', 'snd', 'som', 'sot', 'srd', 'srp', 'sun', 
-                'swa', 'tam', 'tat', 'tay', 'tir', 'tkl', 'tly', 'tpi', 
-                'tsn', 'tuk', 'tvl', 'twi', 'tyv', 'udm', 'uig', 'umb', 
-                'unk', 'uzb', 'wbp', 'wol', 'wuu', 'xal', 'xho', 'xmf', 
-                'yap', 'yij', 'yor', 'yua', 'zha', 'zul', 'zza']
+ADAPTED = ['aar', 'abk', 'abq', 'ace', 'ach', 'ady', 'afr', 'agr', 
+                'aka', 'akl', 'akz', 'ale', 'alt', 'ami', 'aqc', 'ara', 
+                'arg', 'arw', 'arz', 'asm', 'ava', 'aym', 'aze', 'bak', 
+                'bal', 'bam', 'bcl', 'bel', 'ben', 'bis', 'bod', 'bos', 
+                'bre', 'bua', 'bug', 'bul', 'cat', 'ceb', 'ces', 'cha', 
+                'che', 'chk', 'chm', 'cho', 'chv', 'cic', 'cjs', 'cor', 
+                'crh', 'cym', 'dan', 'dar', 'deu', 'dsb', 'eng', 'est', 
+                'eus', 'ewe', 'fao', 'fas', 'fij', 'fil', 'fin', 'fra', 
+                'frr', 'fry', 'fur', 'gaa', 'gag', 'gla', 'gle', 'glg', 
+                'grc', 'grn', 'gsw', 'guj', 'hak', 'hat', 'hau', 'haw', 
+                'hbs', 'heb', 'hil', 'hin', 'hit', 'hrv', 'hun', 'iba', 
+                'ilo', 'ind', 'inh', 'isl', 'ita', 'jam', 'jav', 'kaa', 
+                'kab', 'kal', 'kan', 'kaz', 'kbd', 'kea', 'ket', 'khb', 
+                'kin', 'kir', 'kjh', 'kom', 'kum', 'kur', 'lat', 'lav', 
+                'lin', 'lit', 'lld', 'lug', 'luo', 'lus', 'lzz', 'mah', 
+                'mal', 'mar', 'mkd', 'mlg', 'mlt', 'mnk', 'mns', 'moh', 
+                'mon', 'mri', 'msa', 'mus', 'mww', 'mya', 'myv', 'mzn', 
+                'nah', 'nap', 'nau', 'nci', 'nds', 'nep', 'new', 'nia', 
+                'niu', 'nld', 'nob', 'non', 'nor', 'nso', 'oci', 'oss', 
+                'osx', 'pag', 'pam', 'pan', 'pau', 'pol', 'pon', 'por', 
+                'ppl', 'prs', 'pus', 'que', 'roh', 'rom', 'ron', 'rtm', 
+                'rus', 'ryu', 'sac', 'sah', 'san', 'sat', 'scn', 'sei', 
+                'slv', 'sme', 'sna', 'snd', 'som', 'sot', 'spa', 'sqi', 
+                'srd', 'srp', 'sun', 'swa', 'swe', 'tam', 'tat', 'tay', 
+                'tel', 'tgk', 'tgl', 'tir', 'tkl', 'tly', 'tpi', 'tsn', 
+                'tuk', 'tur', 'tvl', 'twi', 'tyv', 'udm', 'uig', 'ukr', 
+                'umb', 'unk', 'urd', 'uzb', 'vie', 'wbp', 'wol', 'wuu', 
+                'xal', 'xho', 'xmf', 'yap', 'yid', 'yij', 'yor', 'yua', 
+                'yue', 'zha', 'zho', 'zul', 'zza']
 
-def read_data(path, languages=None, scripts=None):
+def read_data(path, languages=None, scripts=None, mode='wiktionary'):
     """
     path: location of one of Deri & Knight's pronunciation tables
     languages: languages to take from the data (default: take all)
@@ -55,12 +64,20 @@ def read_data(path, languages=None, scripts=None):
     # perhaps different processing of the converters will be necessary
     # if I do anything with diacritics
     # also if I clean the wiktionary data
-    df = pd.read_csv(path, sep='\t', 
-                names=['lang', 'script', 'spelling', 'ipa', 'raw_ipa'],
-                usecols=['lang', 'script', 'spelling', 'ipa'],
-                converters={'spelling': lambda word: ' '.join(w.lower().strip() for w in word)},
-                na_filter=False, #because there's a language with ISO 639-3 code nan
-                encoding='utf-8')
+    assert mode in {'wiktionary', 'ipahelp'}, mode
+    if mode == 'wiktionary':
+        df = pd.read_csv(path, sep='\t', 
+                    names=['lang', 'script', 'spelling', 'ipa', 'raw_ipa'],
+                    usecols=['lang', 'script', 'spelling', 'ipa'],
+                    converters={'spelling': lambda word: ' '.join(w.lower().strip() for w in word)},
+                    na_filter=False, #because there's a language with ISO 639-3 code nan
+                    encoding='utf-8')
+    else:
+        df = pd.read_csv(path, sep='\t', 
+                    names=['lang', 'ignore', 'spelling', 'script', 
+                    'ipa', 'prob'], usecols=['lang', 'script', 
+                    'spelling', 'ipa'])
+
     selected_langs = select_languages(df, languages) # sorta spaghetti
     selected_langs_and_scripts = select_scripts(selected_langs, scripts)
     return selected_langs_and_scripts
@@ -120,12 +137,18 @@ def partition_data(df, validation_size):
     print(validation['lang'].unique().size)
     return train, validation
 
-def generate_pron_data(languages, scripts):
+def generate_pron_data(languages, scripts, mode):
+    assert mode in {'wiktionary', 'ipahelp', 'unioned'}
     if languages == ['high']:
         languages = HIGH_RESOURCE #a little hacky
-    elif languages == ['all_lang']:
-        languages = HIGH_RESOURCE + LOW_RESOURCE
-    train_and_validate = sample(read_data(TRAINING_DATA_PATH, languages, scripts), 10000)
+    elif languages == ['adapted']:
+        languages = ADAPTED
+    train_and_validate = sample(read_data(WIKTIONARY_TRAINING_PATH, languages, scripts, 'wiktionary'), 10000)
     train, validate = partition_data(train_and_validate, 0.1)
+    print(train.head())
+    if mode == 'ipahelp':
+        train = train.reset_index().append(read_data(IPAHELP_PATH, languages, scripts, mode='ipahelp'))
+        print(train.head())
+    
     test = read_data(TEST_DATA_PATH, languages, scripts)
     return train, validate, test
