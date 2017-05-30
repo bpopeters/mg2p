0e4fe7d253b14e7ebf60453ed1cd2d4fa44a31d3
Benjamin Peters <bpeters@coli.uni-saarland.de>
2017-05-30 15:54:14 +0200

    Added WER 100, although in a pretty ugly way. evaluate should be overhauled

--- tools/evaluate.py
+++ tools/evaluate.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 
 """
-Functions for measuring and visualizing the performance of g2p models.
-
-todo: functions for comparing one model's results to another's
+Functions for measuring the performance of g2p models.
 """
 
-from os.path import join
+from os.path import join, dirname
 import pandas as pd
 import numpy as np
 import argparse
+import sys
 
 HIGH_RESOURCE = ['ady', 'afr', 'ain', 'amh', 'ang', 'ara', 'arc', 'ast', 
                 'aze', 'bak', 'ben', 'bre', 'bul', 'cat', 'ces', 'cym', 
@@ -52,6 +51,32 @@ ADAPTED = ['aar', 'abk', 'abq', 'ace', 'ach', 'ady', 'afr', 'agr',
                 'umb', 'unk', 'urd', 'uzb', 'vie', 'wbp', 'wol', 'wuu', 
                 'xal', 'xho', 'xmf', 'yap', 'yid', 'yij', 'yor', 'yua', 
                 'yue', 'zha', 'zho', 'zul', 'zza']
+                
+UNSEEN = ['ruo', 'kmv', 'nhv', 'kum', 'ota', 'nau', 'eto', 'afb', 'lug', 
+        'apy', 'pam', 'gnc', 'gez', 'gaa', 'nch', 'ave', 'mlv', 'kmg', 
+        'tkl', 'pms', 'iku', 'sea', 'rgn', 'pnw', 'pny', 'jam', 'gmh', 
+        'vec', 'yij', 'nov', 'prs', 'kal', 'lkt', 'pis', 'pro', 'pac', 
+        'mns', 'lac', 'sty', 'lld', 'cqd', 'sgs', 'bor', 'nso', 'meo', 
+        'ntj', 'hoi', 'rif', 'abq', 'div', 'chc', 'aln', 'dbl', 'alq', 
+        'ckt', 'aot', 'stq', 'asm', 'liv', 'blc', 'niu', 'gub', 'hau', 
+        'rup', 'nij', 'nab', 'mco', 'mwl', 'rmo', 'fur', 'jav', 'dng', 
+        'srn', 'ace', 'kir', 'cho', 'yor', 'pcd', 'sun', 'yap', 'tyz', 
+        'pdt', 'inh', 'tly', 'msn', 'amn', 'sva', 'cim', 'agr', 'wym', 
+        'nep', 'orv', 'pjt', 'srd', 'ude', 'tsd', 'hit', 'nio', 'csi', 
+        'bdq', 'kea', 'nhn', 'hat', 'vma', 'pag', 'ssf', 'kut', 'twi', 
+        'mah', 'crs', 'cal', 'niv', 'kom', 'lin', 'aqc', 'frr', 'sna', 
+        'gwi', 'ext', 'rtm', 'tir', 'mnk', 'rad', 'aii', 'yii', 'snd', 
+        'avd', 'fil', 'crg', 'evn', 'abe', 'dar', 'kjh', 'abz', 'aak', 
+        'bio', 'ing', 'mop', 'zai', 'new', 'abt', 'gbb', 'nxn', 'mus', 
+        'hei', 'apn', 'wgy', 'bzg', 'kxo', 'bam', 'gzi', 'chl', 'oji', 
+        'pap', 'shn', 'sjn', 'arw', 'mzn', 'adj', 'led', 'smk', 'akk', 
+        'gqn', 'wwo', 'myp', 'amp', 'ket', 'alr', 'oge', 'hak', 'kpv', 
+        'are', 'szl', 'kky', 'tay', 'nuk', 'lmo', 'nia', 'tqw', 'lus', 
+        'akl', 'wbp', 'xmf', 'blq', 'mvi', 'lzz', 'ckb', 'bug', 'ilo', 
+        'mis', 'xto', 'koy', 'swh', 'aym', 'mga', 'pre', 'axm', 'azn', 
+        'agg', 'pau', 'sje', 'sac', 'che', 'aau', 'taa', 'nhg', 'pdc', 
+        'apc', 'odt', 'iba', 'duj', 'vro', 'fax', 'vls', 'non', 'acw', 
+        'wnw', 'mcm', 'grn', 'mar', 'kuu']
 
 @np.vectorize
 def levenshtein(a, b):
@@ -87,34 +112,21 @@ def wer(results):
     returns: word error rate of the predictions
     """
     return (results['predicted'] != results['gold']).sum() / results['predicted'].size
-        
-'''
-def language_labels(source_file):
-    with open(source_file) as f:
-        return pd.Series([line.split(None, 1)[0] for line in f])
-'''
+    
+def wer100(results):
+    """
+    results: DataFrame containing at minimum columns for top 100 predicted and
+            gold standard phoneme sequences
+    returns: WER 100 of the predictions
+    """
+    return (not_in(results['gold'], results['predicted-100'])).sum() / results['predicted'].size
+    
+def wer1(results):
+    return (not_in(results['gold'], results['predicted-1'])).sum() / results['predicted'].size
         
 def word_series(corpus_file):
     with open(corpus_file) as f:
-        return pd.Series([line.strip().split() for line in f])
-    
-'''
-def output_table(model_path):
-    """
-    model_path: model directory, containing the corpus subdirectory and the
-                results on src.test
-    returns: DataFrame whose rows are (language, gold phonemes, predicted phonemes)
-    """
-    source_test = join(model_path, 'corpus', 'src.test')
-    target_test = join(model_path, 'corpus', 'tgt.test')
-    predicted_test = join(model_path, 'predicted.txt')
-    
-    lang_id = language_labels(source_test)
-    gold_words = read_words(target_test)
-    predicted_words = read_words(predicted_test)
-
-    return pd.DataFrame.from_items([('lang', lang_id), ('gold', gold_words), ('predicted', predicted_words)])
-'''
+        return pd.Series([tuple(line.strip().split()) for line in f])
     
 def corpus_size(data, index_to_use=None):
     """
@@ -141,48 +153,41 @@ def raw_output(model_path):
     
     return pd.DataFrame.from_items([('lang', lang_id), ('gold', gold_words), ('predicted', predicted_words)])
 
-'''    
 def evaluate_single_model(path):
     """
     model_path: model directory, containing the corpus subdirectory and the
                 results on src.test
-    returns: DataFrame containing error rates and the quantity of training data per language
+    returns: DataFrame: rows are for languages and summaries over test sets,
+            columns are for WER and PER
     """
-    # compare the predicted.txt against the tgt.test
-    # also 
+    name_mapper = {'langid-85':'LangID-High', 'langid-229':'LangID-Adapted', 'langid-all':'LangID-All',
+                    'nolangid-85':'NoLangID-High', 'nolangid-229':'NoLangID-Adapted', 'nolangid-all':'NoLangID-All'}
+    raw_name = path.split('/')[1] # change this
+    model_name = name_mapper.get(raw_name, raw_name)
     
-    
-    source_train = join(path, 'corpus', 'src.train')
-    source_test = join(path, 'corpus', 'src.test') 
-    results = raw_output(model_path)
-    training_counts = corpus_size(source_train, results['lang'].unique())
-    test_counts = corpus_size(source_test, results['lang'].unique())
-    phones = results.groupby('lang').apply(per)
-    words = results.groupby('lang').apply(wer)
-    df = pd.DataFrame.from_items([('wer', words), ('per', phones), ('training_size', training_counts), ('test_size', test_counts)])
-    return df
-'''
-
-def evaluate_single_model(path):
-    """
-    model_path: model directory, containing the corpus subdirectory and the
-                results on src.test
-    returns: DataFrame containing error rates and the quantity of training data per language
-    """
-    # compare the predicted.txt against the tgt.test
-    # also 
     predicted = word_series(join(path, 'predicted.txt')) # is it necessary to do it like this?
+    predicted_1 = read_nbest(join(path, 'predicted-100.txt'), n=1)
+    predicted_100 = read_nbest(join(path, 'predicted-100.txt'), n=100)
     gold = word_series(join(path, 'corpus', 'tgt.test'))
-    lang_id = pd.read_csv(join(path, 'corpus', 'lang_index.test'), header=None).squeeze()
+    lang_id = pd.read_csv(join(path, 'corpus', 'lang_index.test'), na_filter=False, header=None).squeeze()
     
-    results = pd.DataFrame.from_items([('lang', lang_id), ('gold', gold), ('predicted', predicted)])
-    #training_counts = corpus_size(source_train, results['lang'].unique())
-    #test_counts = corpus_size(source_test, results['lang'].unique())
+    results = pd.DataFrame.from_items([('lang', lang_id), ('gold', gold), ('predicted', predicted), ('predicted-100', predicted_100), ('predicted-1', predicted_1)])
+    #results.loc[results['lang'] == 'spa',:]
     phones = results.groupby('lang').apply(per)
     words = results.groupby('lang').apply(wer)
-    #df = pd.DataFrame.from_items([('wer', words), ('per', phones), ('training_size', training_counts), ('test_size', test_counts)])
-    df = pd.DataFrame.from_items([('wer', words), ('per', phones)])
+    words1 = results.groupby('lang').apply(wer1)
+    words100 = results.groupby('lang').apply(wer100)
+    df = pd.DataFrame.from_items([('WER', words1), ('WER 100', words100), ('PER', phones)])
+    df = df * 100
+    df.loc['all',:] = df.mean() # seems to work
+    df.loc['high resource',:] = df.loc[HIGH_RESOURCE,:].mean()
+    df.loc['adapted',:] = df.loc[ADAPTED,:].mean()
+    df.loc['unseen',:] = df.loc[UNSEEN,:].mean()
+    df['model'] = model_name
     return df
+    
+def exp_result_table(experiment_results, test_set):
+    print(experiment_results.groupby('model').apply(lambda df: df.loc[test_set,['WER', 'WER 100', 'PER']]).to_latex(float_format='%.2f'))
     
 
 def evaluate(models):
@@ -195,22 +200,33 @@ def evaluate(models):
             Deri and Knight's paper for details), and on the entire test
             set.
     """
-    # so, there's something in here that needs to be several times:
-    # gotta read in each model and measure its results
+    return pd.concat([evaluate_single_model(path) for path in models])
     
-    # not done
-    return pd.concat([evaluate_single_model(path) for path in models], axis=1)
+def read_nbest(path, beam=100, n=100):
+    with open(path) as f:
+        args = [(tuple(line.strip().split()) for line in f)] * beam
+        return pd.Series(list(zip(*args))).apply(lambda seq: set(seq[:n]))
+        
+@np.vectorize
+def not_in(x, y):
+    return x not in y
     
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('models', nargs='*', default=[], help="Paths to models")
     opt = parser.parse_args()
-    #languages = ['<{}>'.format(lang) for lang in ADAPTED]
-    #languages = ['<{}>'.format(lang) for lang in HIGH_RESOURCE]
     
     model_stats = evaluate(opt.models)
+    exp_result_table(model_stats, 'adapted')
+    exp_result_table(model_stats, 'high resource')
+    exp_result_table(model_stats, 'unseen')
+    model_stats.to_csv('all_results.csv', sep='\t', float_format='%.3f')
+    
+if __name__ == '__main__':
+    main()
     '''
-    model_stats = model_stats.loc[languages,:]
-    print(model_stats.mean())
-    model_stats.sort_values(by='per').to_csv(join(model_path, 'results.csv'), sep='\t')
+    path = '/home/bpop/thesis/mg2p/models/langid-all'
+    predicted_100 = read_nbest(join(path, 'predicted.txt'), n=100)
+    gold = word_series(join(path, 'corpus', 'tgt.test'))
     '''
+        
