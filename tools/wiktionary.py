3efb284f8389a7a27d1fd5c478ce0c07f537bb0d
Benjamin Peters <bpeters@coli.uni-saarland.de>
2017-06-09 15:37:40 +0200

    Removed unused function

--- tools/wiktionary.py
+++ tools/wiktionary.py
@@ -12,6 +12,48 @@ WIKTIONARY_TRAINING_PATH = '/home/bpop/thesis/mg2p/data/deri-knight/pron_data/go
 TEST_DATA_PATH = '/home/bpop/thesis/mg2p/data/deri-knight/pron_data/gold_data_test'
 IPAHELP_PATH = '/home/bpop/thesis/mg2p/data/deri-knight/ipa_help/all.g-to-ipa.cleaned.table'
 
+HIGH_RESOURCE = ['ady', 'afr', 'ain', 'amh', 'ang', 'ara', 'arc', 'ast', 
+                'aze', 'bak', 'ben', 'bre', 'bul', 'cat', 'ces', 'cym', 
+                'dan', 'deu', 'dsb', 'ell', 'eng', 'epo', 'eus', 'fao', 
+                'fas', 'fin', 'fra', 'gla', 'gle', 'hbs', 'heb', 'hin', 
+                'hun', 'hye', 'ido', 'isl', 'ita', 'jbo', 'jpn', 'kat', 
+                'kbd', 'kor', 'kur', 'lao', 'lat', 'lav', 'lit', 'ltz', 
+                'mkd', 'mlt', 'msa', 'mya', 'nan', 'nci', 'nld', 'nno', 
+                'nob', 'oci', 'pol', 'por', 'pus', 'ron', 'rus', 'san', 
+                'scn', 'sco', 'sga', 'slk', 'slv', 'spa', 'sqi', 'swe', 
+                'syc', 'tel', 'tgk', 'tgl', 'tha', 'tur', 'ukr', 'urd', 
+                'vie', 'vol', 'yid', 'yue', 'zho']
+                
+ADAPTED = ['aar', 'abk', 'abq', 'ace', 'ach', 'ady', 'afr', 'agr', 
+                'aka', 'akl', 'akz', 'ale', 'alt', 'ami', 'aqc', 'ara', 
+                'arg', 'arw', 'arz', 'asm', 'ava', 'aym', 'aze', 'bak', 
+                'bal', 'bam', 'bcl', 'bel', 'ben', 'bis', 'bod', 'bos', 
+                'bre', 'bua', 'bug', 'bul', 'cat', 'ceb', 'ces', 'cha', 
+                'che', 'chk', 'chm', 'cho', 'chv', 'cic', 'cjs', 'cor', 
+                'crh', 'cym', 'dan', 'dar', 'deu', 'dsb', 'eng', 'est', 
+                'eus', 'ewe', 'fao', 'fas', 'fij', 'fil', 'fin', 'fra', 
+                'frr', 'fry', 'fur', 'gaa', 'gag', 'gla', 'gle', 'glg', 
+                'grc', 'grn', 'gsw', 'guj', 'hak', 'hat', 'hau', 'haw', 
+                'hbs', 'heb', 'hil', 'hin', 'hit', 'hrv', 'hun', 'iba', 
+                'ilo', 'ind', 'inh', 'isl', 'ita', 'jam', 'jav', 'kaa', 
+                'kab', 'kal', 'kan', 'kaz', 'kbd', 'kea', 'ket', 'khb', 
+                'kin', 'kir', 'kjh', 'kom', 'kum', 'kur', 'lat', 'lav', 
+                'lin', 'lit', 'lld', 'lug', 'luo', 'lus', 'lzz', 'mah', 
+                'mal', 'mar', 'mkd', 'mlg', 'mlt', 'mnk', 'mns', 'moh', 
+                'mon', 'mri', 'msa', 'mus', 'mww', 'mya', 'myv', 'mzn', 
+                'nah', 'nap', 'nau', 'nci', 'nds', 'nep', 'new', 'nia', 
+                'niu', 'nld', 'nob', 'non', 'nor', 'nso', 'oci', 'oss', 
+                'osx', 'pag', 'pam', 'pan', 'pau', 'pol', 'pon', 'por', 
+                'ppl', 'prs', 'pus', 'que', 'roh', 'rom', 'ron', 'rtm', 
+                'rus', 'ryu', 'sac', 'sah', 'san', 'sat', 'scn', 'sei', 
+                'slv', 'sme', 'sna', 'snd', 'som', 'sot', 'spa', 'sqi', 
+                'srd', 'srp', 'sun', 'swa', 'swe', 'tam', 'tat', 'tay', 
+                'tel', 'tgk', 'tgl', 'tir', 'tkl', 'tly', 'tpi', 'tsn', 
+                'tuk', 'tur', 'tvl', 'twi', 'tyv', 'udm', 'uig', 'ukr', 
+                'umb', 'unk', 'urd', 'uzb', 'vie', 'wbp', 'wol', 'wuu', 
+                'xal', 'xho', 'xmf', 'yap', 'yid', 'yij', 'yor', 'yua', 
+                'yue', 'zha', 'zho', 'zul', 'zza']
+
 def read_data(path, languages=None, scripts=None):
     """
     path: location of one of Deri & Knight's pronunciation tables
@@ -30,12 +72,6 @@ def read_data(path, languages=None, scripts=None):
     selected_langs = select_rows(df, 'lang', languages)
     selected_langs_and_scripts = select_rows(selected_langs, 'script', scripts)
     return selected_langs_and_scripts
-    
-def read_ipahelp(path):
-    return pd.read_csv(path, sep='\t', 
-                    names=['lang', 'ignore', 'spelling', 'script', 
-                    'ipa', 'prob'], usecols=['lang', 'script', 
-                    'spelling', 'ipa'])
     
 def select_rows(df, column, values):
     if values is not None:
